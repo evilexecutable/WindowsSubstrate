b5d3ef3abe01b3b596e44096d694011060f7f91b
Lewis Smith <lewis.smith543@gmail.com>
2019-02-03 16:43:55 +0000

    Create method for returning process base address.

--- WindowsSubstrate/Substrate.cs
+++ WindowsSubstrate/Substrate.cs
@@ -11,24 +11,58 @@ namespace WindowsSubstrate
 {
     public class Substrate
     {
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            PROCESS_VM_READ = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            PROCESS_QUERY_INFORMATION = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
         //Import the user32.dll at the correct function and make
         //the function public.
-        [DllImport("user32.dll", EntryPoint = "SetWindowText", CharSet = CharSet.Ansi)]
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Ansi)]
         public static extern int SetWindowText(IntPtr hWnd, String text);
+        //public static extern int WriteProcessMemory(IntPtr hProcess, String lpBaseAddress, String lpBuffer);
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Ansi)]
+        public static extern int FindWindow(String ClassName, String WindowName);
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Ansi)]
+        public static extern uint GetWindowThreadProcessId(int hWnd, IntPtr ProcessId);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(ProcessAccessFlags processAccess, bool bInheritHandle, int processId);
 
+        //Simple method for changing Window Title.
         public void ChangeWindowTitle(String ProcessName, String NewTitle)
         {
             //Get the chosen process and see if it exists.
-            Process[] ChosenProcesses = Process.GetProcessesByName(ProcessName);
-            if (ChosenProcesses.Length > 0)
+            Process[] ChosenProcess = Process.GetProcessesByName(ProcessName);
+            if (ChosenProcess.Length > 0)
             {
                 //Call the function we imported earlier.
-                SetWindowText(ChosenProcesses[0].MainWindowHandle, NewTitle);
+                SetWindowText(ChosenProcess[0].MainWindowHandle, NewTitle);
                 Console.WriteLine("Process " + ProcessName + " title changed to " + NewTitle);
             } else
             {
                 Console.WriteLine("Process not found.");
             }
+        }
+
+        //Get the passed process name and fetch its base address.
+        public Int64 GetBaseAddress(string NewProcessName)
+        {
+            Process[] processes = System.Diagnostics.Process.GetProcessesByName(NewProcessName);
+            Int64 baseAddr = processes[0].MainModule.BaseAddress.ToInt64();
+
+            //Return the base address to the call as an 64bit Int.
+            return baseAddr;
         }
     }
 }
